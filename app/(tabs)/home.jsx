68519e447efd70c945f615ff9b3e563776ef86ee
Min Set <minn.set@gmail.com>
2024-07-08 16:13:47 +0700

    imported images

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,11 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, ScrollView } from 'react-native'
+import { useState } from 'react'
+import FormField from '../../components/FormField'
 
 const home = () => {
+
+
+  const [form, setForm] = useState({ email: '', password: '' })
+
   return (
-    <View>
-      <Text>home</Text>
-    </View>
+    <SafeAreaView className="h-full">
+      <ScrollView>
+      <FormField
+            title="Email"
+            value={form.email}
+            handleChangeText={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
